60178a3bb5cb02a06b0d7e43aec89a75b99eb644
Moksha-132 <lakshmimoksha.132@gmail.com>
2026-06-04 11:56:49 +0530

    Group below-the-fold components into a single chunk to massively reduce main thread blocking time

---
+++ src/components/HomePageContent.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Banner from './Banner';
+import Partners from './Partners';
+import News from './News';
+import Testimonial from './Testimonial';
+
+const HomePageContent = () => {
+    return (
+        <>
+            <Banner />
+            <Partners />
+            <News />
+            <Testimonial />
+        </>
+    );
+};
+
+export default HomePageContent;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,14 @@
 import React, { lazy, Suspense } from 'react';
 import Hero from '../components/Hero';
 
-const Banner = lazy(() => import('../components/Banner'));
-const Partners = lazy(() => import('../components/Partners'));
-const News = lazy(() => import('../components/News'));
-const Testimonial = lazy(() => import('../components/Testimonial'));
+const HomePageContent = lazy(() => import('../components/HomePageContent'));
 
 const HomePage = () => {
     return (
         <>
             <Hero />
             <Suspense fallback={<div style={{ minHeight: '300vh', background: 'var(--background)' }}></div>}>
-                <Banner />
-                <Partners />
-                <News />
-                <Testimonial />
+                <HomePageContent />
             </Suspense>
         </>
     );
